4e1b37252cc607315b63d3a6774c08479de98599
Andrey <scorpion.and@ukr.net>
2017-07-18 11:34:40 +0300

    Change mobile view and image to sm view

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,16 @@ gulp.task('browser-sync', function() {
     });
 });
 
+gulp.task('dist', function() {
+    browserSync({
+        server: {
+            baseDir: 'dist'
+        },
+        notify: false
+    });
+});
+
+
 gulp.task('pug', function () {
     return gulp.src('./src/pug/index.pug')
         .pipe(pug({
@@ -35,13 +45,20 @@ gulp.task('pug', function () {
         .pipe(browserSync.reload({stream: true}));
 });
 
+gulp.task('font', function() {
+    return gulp.src('src/libs/font-awesome/css/font-awesome.min.css')
+                .pipe(gulp.dest('src/css/'));
+});
+
 gulp.task('bootstrap--var', function() {
     return gulp.src('src/libs/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss')
+                .pipe(concat('libs.scss'))
                 .pipe(gulp.dest('src/sass/'))
 });
 
-gulp.task('bootstrap--js', function() {
-    return gulp.src('src/libs/jquery/dist/jquery.min.js')
+gulp.task('lib--js', function() {
+    return gulp.src(['src/libs/jquery/dist/jquery.slim.min.js','src/libs/bootstrap-sass/assets/javascripts/bootstrap/carousel.js','src/libs/bootstrap-sass/assets/javascripts/bootstrap/collapse.js'])
+                .pipe(concat('libs.js'))
                 .pipe(gulp.dest('src/js/'));
 });
 
@@ -52,40 +69,40 @@ gulp.task('bootstrap--js', function() {
 //         .pipe(gulp.dest('app/sass'))
 // });
 
-// // min js
-// gulp.task('js-min', function() {
-//     return gulp.src('app/js/**/*.js')
-//         .pipe(uglify())
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(gulp.dest('dist/js'));
-// });
-//
-//
-// // min css
-// gulp.task('cssmin', function() {
-//     return gulp.src('app/css/main.css')
-//         .pipe(cssmin())
-//         .pipe(rename({suffix: '.min'}))
-//         .pipe(gulp.dest('dist/css'));
-// });
-//
-// // image min task
-// gulp.task('img', function() {
-//     return gulp.src('app/img/*')
-//         .pipe(imagemin())
-//         .pipe(gulp.dest('dist/img'));
-// });
-//
-//
-// // html
-// gulp.task('html', function() {
-//     return gulp.src('app/*.html')
-//         .pipe(gulp.dest('dist'));
-// })
-//
-//
-// // bild project to dist
-// gulp.task('bild',['html','cssmin','js-min','img']);
+// min js
+gulp.task('js-min', function() {
+    return gulp.src('src/js/**/*.js')
+        .pipe(uglify())
+        // .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/js'));
+});
+
+
+// min css
+gulp.task('cssmin', function() {
+    return gulp.src('src/css/main.css')
+        .pipe(cssmin())
+        // .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// image min task
+gulp.task('img', function() {
+    return gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+
+// html
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+})
+
+
+// bild project to dist
+gulp.task('bild',['html','cssmin','js-min','img']);
 
 
 gulp.task('watch', ['pug', 'sass', 'browser-sync'], function() {
